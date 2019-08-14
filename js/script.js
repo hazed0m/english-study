2790c00cd096546a8a936010921dc3d5f697921e
Graalex <41749146+hazed0m@users.noreply.github.com>
2019-08-14 16:33:49 +0300

    desktop + js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#main .main-form input[type='tel'] , #skype-consult .skype-form input[type='tel']").mask("999-99-99");
+    $("#main .main-form input[type='tel'] , #skype-consult .skype-form input[type='tel'], #free-consult .free-consult-form input[type='tel']").mask("999-99-99");
     $('img[src$=".svg"]').each(function() {
         var $img = jQuery(this);
         var imgURL = $img.attr('src');
@@ -21,31 +21,90 @@ $(document).ready(function() {
             $img.replaceWith($svg);
         }, 'xml');
     });
-});
-$('.tel-code-wrapper').on('click',function(){
-    if($(this).find('.code-list').is(':visible'))
-    {
-        $(this).find('.code-list').fadeOut();
-    }
-    else
-    {
-        $(this).find('.code-list').fadeIn();
-    }
-});
-$('.code-list .code-item').on('click',function(){
-    $('.tel-code-wrapper .current-code #code').text($(this).text());
-    $('.tel-code-wrapper').find('.code-list').fadeOut();    
-});
-$('#advantages .toogle-wrapper .toogle-item').on('click',function(){
-    if($(this).find('.toogle-text').is(':visible'))
-    {
-        $(this).find('.toogle-text').slideUp();
-        setTimeout(() => {
-            $(this).removeClass('active');
-        },300);
-    }
-    else
-    {
-        $(this).addClass('active').find('.toogle-text').slideDown();
-    }
+    $(".menu-wrapper").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+    let position = $(window).scrollTop(); 
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if(scroll > position) {            
+            if(position == 0)
+            {
+                let top = $('#about').offset().top;
+                $('body,html').animate({scrollTop: top}, 1500);
+            }
+        }
+        position = scroll;
+    });
+    $('.up-handler').on('click',function(){
+        $('body,html').animate({scrollTop: 0}, 1500);
+    });
+    $('.tel-code-wrapper').on('click',function(){
+        if($(this).find('.code-list').is(':visible'))
+        {
+            $(this).find('.code-list').fadeOut();
+        }
+        else
+        {
+            $(this).find('.code-list').fadeIn();
+        }
+    });
+    $('.code-list .code-item').on('click',function(){
+        $('.tel-code-wrapper .current-code #code').text($(this).text());
+        $('.tel-code-wrapper').find('.code-list').fadeOut();    
+    });
+    $('#advantages .toogle-wrapper .toogle-item').on('click',function(){
+        if($(this).find('.toogle-text').is(':visible'))
+        {
+            $(this).find('.toogle-text').slideUp();
+            setTimeout(() => {
+                $(this).removeClass('active');
+            },300);
+        }
+        else
+        {
+            $(this).addClass('active').find('.toogle-text').slideDown();
+        }
+    });
+    $('#stages .toogle-but').on('click',function(){
+        let currentId = this.id;
+        $('#stages .toogle-but.active').removeClass('active');
+        $(this).addClass('active');
+        $(`.info-wrapper.active`).removeClass('active').fadeOut();
+        $(`.info-wrapper#${currentId}`).addClass('active').fadeIn();
+        setTimeout(function(){
+            $(`.info-wrapper#${currentId}`).css('display','flex');
+        },400);
+    });
+    $("#teachers .owl-carousel").owlCarousel({
+        center: true,
+        items:5,
+        loop:true,
+        nav: true,
+        navText: [`<i class="fas fa-chevron-left"></i>`,`<i class="fas fa-chevron-right"></i>`],
+        navSpeed:800,
+        responsive:{
+            600:{
+                items:4
+            }
+        }
+    });
+    $('#reviews .owl-carousel').owlCarousel({
+        items:3,
+        autoWidth:true,
+        nav: true,
+        navText: [`<i class="fas fa-chevron-left"></i>`,`<i class="fas fa-chevron-right"></i>`],
+        navSpeed:800,
+        margin:50,
+        animateIn:'ease-in-out',
+        animateOut:'ease-in-out',
+        responsive:{
+            600:{
+                items:4
+            }
+        }
+    });
 });
